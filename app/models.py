1a68b97a2f8465908b78d56a119a6b977c14aa95
Nick <nakalvarez2012@gmail.com>
2024-04-23 21:07:02 +0700

    removed phone from users

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    phone_number = Column(String)
+    #phone_number = Column(String)
 
 # class User(Base):
 #     """
